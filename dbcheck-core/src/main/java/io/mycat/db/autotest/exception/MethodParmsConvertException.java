cfaff12d0547328b6ad7ae7cd25b529fa8dfea65
zjliu <qiankunshe@126.com>
2017-03-01 17:09:11 +0800

    调整了目录名称

--- dbcheck-core/src/main/java/io/mycat/db/autotest/exception/MethodParmsConvertException.java
+++
@@ -1,7 +0,0 @@
-package io.mycat.db.autotest.exception;
-
-public class MethodParmsConvertException extends RuntimeException {
-    public MethodParmsConvertException(String msg, Throwable cause) {
-		super(msg, cause);
-	}
-}
--- dbcheck-core/src/test/resources/test/查询/cleanData.sql
+++
@@ -1,1 +0,0 @@
-delete from test where test_id in ('1','2','3','4','5','6','7','8','9','10');
